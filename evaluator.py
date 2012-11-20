9a6cf6491037ae808eedf439eec36a9c6c2fe1da
Kenya <kenya.vs@gmail.com>
2012-11-20 16:35:57 -0500

    environemnt needs work

--- evaluator.py
+++ evaluator.py
@@ -7,25 +7,29 @@ from collections import Iterable
 variables = {}
 fun_path = []
 
-operators = {'+': lambda t: plus(t),
-             '-': lambda t: sub(t),
-             '*': lambda t: mul(t),
-             '/': lambda t: div(t),
-             'var': lambda t: assign(t),
-             'if': lambda t: cond(t),
-             '&&': lambda t: logical_and(t),
-             '||': lambda t: logical_or(t),
-             '!': lambda t: logical_not(t),
-             '==': lambda t: equality(t),
-             '<=': lambda t: less_eq(t),
-             '>=': lambda t: great_eq(t),
-             '<': lambda t: less(t),
-             '>': lambda t: great(t),
-             'alert': lambda t: alert(t),
-             'for': lambda t: for_loop(t),
-             'while': lambda t: while_loop(t),
-             'function': lambda t: function(t),
-             'execute': lambda t: execute(t)
+operators = {'+': lambda t, e: plus(t, e),
+             '-': lambda t, e: sub(t, e),
+             '*': lambda t, e: mul(t, e),
+             '/': lambda t, e: div(t, e),
+             'var': lambda t, e: assign(t, e),
+             'if': lambda t, e: cond(t, e),
+             '&&': lambda t, e: logical_and(t, e),
+             '||': lambda t, e: logical_or(t, e),
+             '!': lambda t, e: logical_not(t, e),
+             '==': lambda t, e: equality(t, e),
+             '<=': lambda t, e: less_eq(t, e),
+             '>=': lambda t, e: great_eq(t, e),
+             '<': lambda t, e: less(t, e),
+             '>': lambda t, e: great(t, e),
+             'alert': lambda t, e: alert(t, e),
+             'for': lambda t, e: for_loop(t, e),
+             'while': lambda t, e: while_loop(t, e),
+             'function': lambda t, e: function(t, e),
+             'execute': lambda t, e: execute(t, e),
+             'true': lambda t, e: True,
+             'false': lambda t, e: False
+
+
              }
 
 
@@ -54,7 +58,9 @@ def while_loop(t, env):
     return env
 
 def alert(t, env):
-    print evaluate(t, env)
+    print "alert"
+    print t
+    print sing_eval(t[0], env)
 
 #Boolean expressions
 def less_eq(t, env):
@@ -105,19 +111,25 @@ def function(t, env):
     return env
 
 def execute(t, env):
-
     i = -1
     found = False
-    while i < (len(env)-1) and (not found):
+
+    while (i < (len(env)-1)) and (not found):
         i+=1
-        try env[i][t[0]]:
+        
+
+        #I think that this should probably be an if statement
+        try:
+            print "this is t"
+            print t
+            env[i][t[0]]
             found = True
             eval_this = env[i][t[0]]
+        except KeyError:
+            "yay"
 
     env = [{}] + env
     evaluate(eval_this, env)
-
-
 
 def is_variable(expression):
 
@@ -152,76 +164,102 @@ def sub(t, env):
     return result
 
 def div(t, env):
-    result = 1
-    for i in t:
-        result = sing_eval(i, env)/result
+    result = sing_eval(t[0], env)
+    for i in t[1:]:
+        result = result/sing_eval(i, env)
 
         #result /= evaluate(i)
     return result
 
 def sing_eval(expr, env):
-
-    token = expr[0]
+    if isinstance(expr, list):
+        token = expr[0]
+    else:
+        token = expr
     
     i = -1
     found = False
     while i < (len(env)-1) and (not found):
         i+=1
-        try env[i][token]:
+        try: 
+            env[i][token]
             found = True
+        except:
+            "yay"
     
-    return env[i][token](expr[1:])
+    if found:
+        if len(env)-1 == i:
+            return env[i][token](expr[1:], env)
+        else:
+            return env[i][token]
+    else:
+        return token
 
 
 
 #Core of the program 
-def evaluate(parser_tree, env):
+def evaluate(parser_tree, env=[{}, operators]):
 
     if isinstance(parser_tree[0], list):
         for i in parser_tree:
             env = evaluate(i, env)
-
-    if isinstance(parser_tree, int):
-        return int(parser_tree)
-    elif isinstance(parser_tree, float):
-        return float(parser_tree)
-    elif is_variable(parser_tree):
-        return variables[parser_tree]
-    elif isinstance(parser_tree, str):
-        return parser_tree
     else:
-        print "Somehow we got here? Perhaps at some point we should seriously consider what gets here!"
+        #if isinstance(parser_tree[0], int):
+         #   return int(parser_tree[0])
+        #elif isinstance(parser_tree[0], float):
+        #    return float(parser_tree[0])
+        #if isinstance(parser_tree[0], str):
+        i = -1
+        found = False
+        while i < (len(env)-1) and (not found):
+            i+=1
+            try: 
+                env[i][parser_tree[0]]
+                found = True
+                return env[i][parser_tree[0]](parser_tree[1:], env)
+            except:
+                "continue checking env dictionaries"
+        #return parser_tree[0]
+        sing_eval(parser_tree[0], env)
+        #else:
+        #    print "Somehow we got here? Perhaps at some point we should seriously consider what gets here!"
 
-    if token == 'true':
-        return True
-    elif token == 'false':
-        return False
-    
-    return operators[token](parser_tree)
+        """if token == 'true':
+            return True
+        elif token == 'false':
+            return False"""
+
+        """
+        if parser_tree[0] == 'true':
+            return True
+        else:
+            return False
+        
+        print "this is token"
+        print token
+        return operators[token](parser_tree, env)
+        """
 
 if __name__ == "__main__":
-    #parser_tree = ["+", 4, 3, 6, ["*", 2, 3]]
-    #parser_tree = ["var", "name", "Jane"]
-    #parser_tree = ["if", ["false"], ["*", 2, 3], ['/', 2, 3] ]
     #parser_tree = ["if", ["||", ["false"], ["!",["false"] ] ],["*", 4, 1], ["-", 8, 2] ]
     #parser_tree = ["if", ["==", 2, 2], ["*", 2, 3], ['/', 2, 3] ]
-    #parser_tree = ["alert", "Hello World!"]
-    #parser_tree = ["+", ["if", ["true"], 3, 2], 4]
     #parser_tree = [["var", "x", "2"], "while", ["==", 2, 'x'], [["alert", "Hello world"], "var", 'x', ["+", 'x', 1] ] ]
     #parser_tree = ["for", ["var", "i", 1], ["<=", "i", 10], ["var", "i", ["+", "i", 1]], ["alert", ["+", "i", 5] ]]
     #parser_tree = [["function", "helloworld", ["alert", "HELLO WORLD!"]], ["execute", "helloworld"]]
+    #parser_tree = [["function", "foo", [["function", "bar", ["alert", "HIYA!"] ], ["execute", "bar"] ]  ], ["execute", "foo"]]
     #parser_tree = [["function", "foo", [["var", "tmp1", 6], ["function", "bar", ["alert", "HIYA!"] ], ["execute", "bar"] ]  ], ["execute", "foo"]]
-    parser_tree = [["function", "foo", [["var", "temp", 3], ["alert", ['+', "temp", 2]]]], ["execute", "foo"]]
+    #parser_tree = [["function", "foo", [["var", "temp", 3], ["alert", ['+', "temp", 2]]]], ["execute", "foo"]]
+    #parser_tree = [["function", "foo", [["var", "tmp1", 6], ["function", "bar", [["var", "tmp1", 1], ["var", "tmp2", 2], ["+", "tmp1", "tmp2"], ["alert", "tmp1"], ["alert", "tmp2"], ["alert", "HIYA!"] ]], ["execute", "bar"], ["alert", "tmp1"] ]  ], ["execute", "foo"]]
+    #parser_tree = [["function", "foo", [["var", "tmp1", 6], ["function", "bar", [ ["var", "tmp2", 2], ["+", "tmp1", "tmp2"], ["alert", "tmp1"] ]], ["execute", "bar"], ["alert", "tmp1"] ]  ], ["execute", "foo"]]
     #parser_tree = [["function", "foo", [["alert", ['+', 3, 2]]]], ["execute", "foo"]]
-
+    parser_tree = [["function", "foo", [["var", "tmp1", 6], ["function", "bar", [["var", "tmp2", 2], ["alert", "tmp1"]] ], ["execute", "bar"] ]  ], ["execute", "foo"]]
     #evaluate(parser_tree)
     #map(evaluate, parser_tree)
 
-    envir = [{}, operators]
+   # while len(parser_tree)>0:
+    evaluate(parser_tree)
 
-    while len(parser_tree)>0:
-        envir = evaluate(parser_tree, envir)
-
+    #print sing_eval(['+', 4, 6], envir)
 
     """[["var", "name", "Jane"],["function", "hi", [["alert", "Hi ", "name"],["var", "name", "John"],["alert", "Hi ", "name"]]] ]
     
@@ -247,5 +285,3 @@ if __name__ == "__main__":
 #add eval for single expression and environment
 #environment: list of dictionaries from most local to global (or, well, operators)
 #unittests
-
-#return
